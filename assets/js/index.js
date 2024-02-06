3f9ca11f897833a7688a4980757f639bfe3abbd0
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2024-02-06 16:51:26 +0200

    Example select element

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,5 +15,14 @@ function logMessager() {
 // const unique = document.querySelector("#unique"); //0(1) - медленее, но больше возможностей CSS свойства
 // const btn = document.querySelector("button:last-of-type");
 
-const btns1 = document.getElementsByClassName("btn");
-const btns2 = document.querySelectorAll(".btn");
+// const btns1 = document.getElementsByClassName("btn");
+// const btns2 = document.querySelectorAll(".btn");
+
+const img1 = document.querySelector("img");
+const [img] = document.getElementsByTagName("img");
+
+const [, text] = document.getElementsByClassName("text");
+
+const li2 = document.querySelector("li:nth-child(2)");
+
+const ps = document.querySelectorAll("#main p");
