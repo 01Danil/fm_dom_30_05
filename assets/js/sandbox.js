ff5c174503ab52f83bd5ee1df51a27461cad1028
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2024-02-07 13:39:13 +0200

    Add createAdder

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -19,3 +19,21 @@ console.log(counter.increment());
 console.log(counter.decrement());
 
 const counter2 = createCounter();
+
+// function createAdder(n) {
+// let sum = n;
+//   return function adder(m) {
+//     return n += m;
+//   };
+// }
+
+// function createAdder(n) {
+//   return (m) => (n += m);
+// }
+
+const createAdder = (n) => (m) => (n += m);
+
+const adder = createAdder(100);
+console.log(adder(15));
+console.log(adder(50));
+console.log(adder(10));
