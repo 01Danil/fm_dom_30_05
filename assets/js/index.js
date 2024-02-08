6d803a40eca299eaca1a6a38b5a2c82564347f4f
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2024-02-08 14:27:41 +0200

    Add stopPropagation

--- assets/js/index.js
+++ assets/js/index.js
@@ -35,26 +35,15 @@ updateView();
 const unique = document.getElementById("unique");
 
 function btnUniqueHandlet(e) {
-  e = e || event;
+  // e = e || event;
+  e.stopPropagation();
   console.group();
   console.log("target:", e.target); // Елемент по которому кликнули
   console.log("currentTarget:", e.currentTarget); // Елемент чем обработчик обработал
-  // unique.removeEventListener("click", btnUniqueHandlet);
-  // unique.disabled = true;
-  // console.log("content e.target.innerText:", e.target.innerText);
-  // console.log("content e.target.textContent:", e.target.textContent);
   console.groupEnd();
 }
 
 unique.addEventListener("click", btnUniqueHandlet);
-unique.addEventListener("click", () => {alert(5)}, {
-  capture: true,
-  once: true,
-});
-// unique.dispatchEvent(new MouseEvent("click"));
-
 window.addEventListener("click", btnUniqueHandlet);
 document.addEventListener("click", btnUniqueHandlet);
 document.body.addEventListener("click", btnUniqueHandlet);
-
-// window.dispatchEvent(new MouseEvent("click"));
--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,22 +1,3 @@
 "use strict";
 
-const obj = {
-  prop1: 1,
-  prop2: 2,
-};
 
-// const prop = true ? "prop1" : "prop2";
-
-//console.log(obj[prop])
-
-// function wrap(count = 3) {
-//   let clickCount = 0;
-//   return function thirtClickEvent() {
-//     console.log(++clickCount);
-//     if (clickCount === count) {
-//       unique.removeEventListener("click", thirtClickEvent);
-//       unique.disabled = true;
-//     }
-//   };
-// }
-// unique.addEventListener("click", wrap());
