1928d7ff8c3fbce30d020668bfca4c7dbe251c83
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2024-02-13 17:07:38 +0200

    Add HOMEWORK

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,15 @@
 "use strict";
-const [link] = document.links;
-link.addEventListener("click", (e) => {
-  e.preventDefault();
-});
 
-const form1 = document.getElementById("rootForm");
-const form2 = document.forms["rootForm"];
+const message = [];
 const form = document.forms.rootForm;
-// const inpText = document.forms.rootForm.elements.inputText;
+const list = document.getElementById("list");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log(e.target.elements.inputText.defaultValue);
-  console.dir(e.target.elements.inputText.value);
+  const inputValue = e.target.elements.inputText.value.trim();
+  if (inputValue) {
+    message.push(inputValue);
+    e.target.reset();
+  }
+  console.log(message);
 });
